34434877f916c5106cd91462f9903198e321191e
SARAN ST <saran.st@viewzenlabs.com>
2026-01-27 12:47:40 +0530

    added notes folder

--- middleware.js
+++
@@ -1,6 +0,0 @@
-function middleware_url_printer(req,res,next){
-    console.log(req.url)
-    console.log(req.method)
-    next()
-}
-module.exports= middleware_url_printer;
--- module.js
+++
@@ -1,7 +0,0 @@
-function add(a,b){
-    console.log(a+b);
-}
-function sub(a,b){
-    console.log(a-b);
-}
-module.exports={add,sub}
